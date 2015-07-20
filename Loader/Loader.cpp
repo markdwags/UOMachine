dc5dbed8e7f281a057f6a5e2e2f8ee1f8fe805e8
John Scott <evilgoat@goats.org.au>
2015-07-20 20:13:48 +0800

    New experimental UOS launcher
    Re-purposed RazorRE's Loader.dll to inject UOS.dll directly into the client itself bypassing the need to launch UOS.exe.
    Will probably convert to C# at a later date to maintain single language project.
    
    Please let me know if you have problems with it on your setup.

---
+++ Loader/Loader.cpp
@@ -0,0 +1,137 @@
+// Loader.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
+enum LOADER_ERROR {
+	SUCCESS = 0,
+	NO_OPEN_EXE,
+	NO_MAP_EXE,
+	NO_READ_EXE_DATA,
+	NO_RUN_EXE,
+	NO_ALLOC_MEN,
+	NO_WRITE,
+	NO_VPROTECT,
+	NO_READ,
+	UNKNOWN_ERROR = 99
+};
+
+LOADER_ERROR __declspec(dllexport) __cdecl Load(char *client, char *clientPath, char *dll, char *dllPath, char *dllFunction, char* params, int *pid) {
+	PVOID dataAlloc;
+	CHAR codeBuffer[256];
+	DWORD codePosition = 0;
+	STARTUPINFOA si;
+	PROCESS_INFORMATION pi;
+
+	// Set enviroment variable for QT to find qwindows.dll or will fail to launch.
+	CHAR qtPlatformPath[128];
+	memset(qtPlatformPath, 0, 128);
+	strcat(qtPlatformPath, dllPath);
+	strcat(qtPlatformPath, "\\Platforms\\");
+	SetEnvironmentVariableA("QT_QPA_PLATFORM_PLUGIN_PATH", qtPlatformPath);
+
+	// Add UOS directory to PATH
+	memset(qtPlatformPath, 0, 128);
+	strcat(qtPlatformPath, dllPath);
+	SetEnvironmentVariableA("PATH", qtPlatformPath);
+
+	memset(codeBuffer, 0, 256);
+	memset(&si, 0, sizeof(STARTUPINFOA));
+	memset(&pi, 0, sizeof(PROCESS_INFORMATION));
+
+	if (!CreateProcessA(client, params, 0, 0, 0, CREATE_SUSPENDED, 0, clientPath, &si, &pi)) {
+		int error = GetLastError();
+		return NO_RUN_EXE;
+	}
+
+	CONTEXT context;
+	memset(&context, 0, sizeof(CONTEXT));
+	context.ContextFlags = 0x10000 | 0x01 | 0x02 | 0x04;
+	GetThreadContext(pi.hThread, &context);
+
+	DWORD eip = context.Eip;
+
+	*pid = pi.dwProcessId;
+
+	dataAlloc = VirtualAllocEx(pi.hProcess, 0, 256, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	if (dataAlloc == 0) {
+		return NO_ALLOC_MEN;
+	}
+
+	// write DLL name to buffer
+	PCHAR dllPos = (char*)dataAlloc + codePosition;
+	memcpy(codeBuffer, dll, strlen(dll));
+	codePosition += (strlen(dll) + 1);
+
+	// write DLL function name to buffer
+	PCHAR dllFuncPos = (char*)dataAlloc + codePosition;
+	memcpy(codeBuffer + codePosition, dllFunction, strlen(dllFunction));
+	codePosition += (strlen(dllFunction) + 1);
+
+	PCHAR ptr = codeBuffer + codePosition;
+	memset(ptr, 0x90, sizeof(codeBuffer)-codePosition);
+
+	DWORD codeStart = codePosition + 5;
+
+	HMODULE hModule = LoadLibraryA("kernel32.dll");
+	LPVOID loadLibrary = GetProcAddress(hModule, "LoadLibraryA");
+	LPVOID getProcAddress = GetProcAddress(hModule, "GetProcAddress");
+
+	/* Inject code into client, LoadLibrary's UOS.dll and then calls Install function then JMP's to original EIP */
+	char code[] = {
+		0x9C						 /* 00:00 PUSHFD */,
+		0x60						 /* 01:01 PUSHAD */,
+		0x68, 0x00, 0x00, 0x00, 0x00 /* 2:6 PUSH dllName */,
+		0xB8, 0x00, 0x00, 0x00, 0x00 /* 7:11 MOV eax, LoadLibraryA */,
+		0xFF, 0xD0					 /* 12:13 CALL eax */,
+		0xBB, 0x00, 0x00, 0x00, 0x00 /* 14:18 MOV ebx, dllFunc */,
+		0x53						 /* 19:19 PUSH ebx */,
+		0x50						 /* 20:20 PUSH eax */,
+		0xB9, 0x00, 0x00, 0x00, 0x00 /* 21:25 MOV ecx, GetProcAddress */,
+		0xFF, 0xD1					 /* 26:27 CALL ecx */,
+		0xFF, 0xD0					 /* 28:29 CALL eax */,
+		0x61						 /* 30:30 POPAD */,
+		0x9D						 /* 31:31 POPFD */,
+		0xE9, 0x00, 0x00, 0x00, 0x00 /* 32:36 JMP origEip */ };
+
+	code[3] = (CHAR)(dllPos);
+	code[4] = (CHAR)((DWORD)dllPos >> 8);
+	code[5] = (CHAR)((DWORD)dllPos >> 16);
+	code[6] = (CHAR)((DWORD)dllPos >> 24);
+
+	code[8] = (CHAR)(loadLibrary);
+	code[9] = (CHAR)((DWORD)loadLibrary >> 8);
+	code[10] = (CHAR)((DWORD)loadLibrary >> 16);
+	code[11] = (CHAR)((DWORD)loadLibrary >> 24);
+
+	code[15] = (CHAR)(dllFuncPos);
+	code[16] = (CHAR)((DWORD)dllFuncPos >> 8);
+	code[17] = (CHAR)((DWORD)dllFuncPos >> 16);
+	code[18] = (CHAR)((DWORD)dllFuncPos >> 24);
+
+	code[22] = (CHAR)(getProcAddress);
+	code[23] = (CHAR)((DWORD)getProcAddress >> 8);
+	code[24] = (CHAR)((DWORD)getProcAddress >> 16);
+	code[25] = (CHAR)((DWORD)getProcAddress >> 24);
+
+	char *eipPtr = (char*)((char*)eip - ((char*)dataAlloc + codeStart + sizeof(code)));
+
+	code[33] = (CHAR)(eipPtr);
+	code[34] = (CHAR)((DWORD)eipPtr >> 8);
+	code[35] = (CHAR)((DWORD)eipPtr >> 16);
+	code[36] = (CHAR)((DWORD)eipPtr >> 24);
+
+	memcpy(codeBuffer + codeStart, code, sizeof(code));
+	SIZE_T out = 0;
+	WriteProcessMemory(pi.hProcess, dataAlloc, codeBuffer, 256, &out);
+
+	context.Eip = ((DWORD)dataAlloc + codeStart);
+	SetThreadContext(pi.hThread, &context);
+
+	ResumeThread(pi.hThread);
+
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	return SUCCESS;
+}
--- UO Machine/Misc/SteamLauncher.cs
+++ UO Machine/Misc/SteamLauncher.cs
@@ -23,146 +23,34 @@ using System.Threading;
 using System.Windows;
 using System.IO;
 using System.Net;
+using System.Text;
 using UOMachine.IPC;
 using UOMachine.Resources;
 using EasyHook;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace UOMachine.Misc
 {
-    internal static class SteamLauncher
+    internal unsafe static class SteamLauncher
     {
+        [DllImport("Loader.dll")]
+        private static unsafe extern uint Load(string exe, string path, string dll, string dllpath, string func, string param, out uint pid);
+
         private static object myLock = new object();
 
         public static bool Launch( OptionsData options, out int index )
         {
-            /* Could all seem a bit excessive just to get the pid of the client it creates? */
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.WorkingDirectory = MainWindow.CurrentOptions.UOSFolder;
-            startInfo.FileName = Path.Combine( MainWindow.CurrentOptions.UOSFolder, MainWindow.CurrentOptions.UOSExePath );
-            index = -1;
-            Win32.SafeProcessHandle hProcess;
-            Win32.SafeThreadHandle hThread;
-            uint pid, tid;
-            int clientPid = 0;
+            uint pid;
+            StringBuilder param = new StringBuilder();
+            param.Append(String.Format("-uo \"{0}\" ", options.UOFolder));
+            param.Append(String.Format("-core \"{0}\" ", options.UOSFolder));
+            param.Append(String.Format("-shard \"{0}\" ", options.Server));
+            param.Append(String.Format("-port \"{0}\" ", options.Port));
+            if (options.PatchClientEncryption) param.Append("-encryption \"Yes\"");
 
-            UOM.SetStatusLabel( Strings.Launchingclient );
-            if (Win32.CreateProcess( startInfo, true, out hProcess, out hThread, out pid, out tid ))
-            {
-                /* Basically rewrites a small piece of code before the client resumes to write the pid of the new client it creates, probably not portable between versions if the registers change, what can i say, I'm a noob
-                 * Never had code of old code before RazorLauncher to see how xenoglyph did it before.
-                 * Code will need to be cleaned up. */
-
-                Process p = Process.GetProcessById( (int)pid );
-                IntPtr codeAddress;
-
-                IntPtr baseAddress = GetBaseAddress( hProcess, hThread );
-                if (baseAddress == null)
-                {
-                    UOM.SetStatusLabel( Strings.UOSteamlaunchfailed );
-                    return false;
-                }
-
-                byte[] buffer = new byte[0x17000];
-                Memory.Read( hProcess.DangerousGetHandle(), (IntPtr)(((int)baseAddress) + 0x1000), buffer, true );
-
-                UOM.SetStatusLabel( Strings.Patchingclient );
-
-                /* Originally pushes hThread and hProcess on the stack before a call, we'll overwrite here. */
-                byte[] findBytes = new byte[] { 0x8B, 0x44, 0x24, 0x44, // MOV EAX, [ESP+44]
-                                                0x8B, 0x4C, 0x24, 0x40  // MOV ECX, [ESP+40]
-                };
-
-                int offset = 0;
-                if (Memory.FindSignatureOffset( findBytes, buffer, out offset ))
-                {
-                    if ((codeAddress = Memory.Allocate( hProcess.DangerousGetHandle(), IntPtr.Zero, 1024, true )) == IntPtr.Zero)
-                    {
-                        UOM.SetStatusLabel( Strings.Memoryallocationfailed );
-                        hProcess.Dispose();
-                        hThread.Dispose();
-                        return false;
-                    }
-
-                    byte[] patchCode1 = new byte[] { 0xB8, 0x00, 0x00, 0x00, 0x00,  // MOV EAX, <codeAddress> 
-                                                 0x8B, 0x4C, 0x24, 0x4C,        // MOV ECX, [ESP+4C]
-                                                 0x89, 0x08,                    // MOV [EAX], ECX
-                                                 0xB8, 0x00, 0x00, 0x00, 0x00,  // MOV EAX, <codeAddress+4>
-                                                 0x8B, 0x4C, 0x24, 0x44,        // MOV ECX, [ESP+4C]
-                                                 0x89, 0x08,                    // MOV [EAX], ECX
-                                                 0x8B, 0x44, 0x24, 0x48,        // MOV EAX, [ESP+48] -.
-                                                 0x8B, 0x4C, 0x24, 0x44,        // MOV ECX, [ESP+44] -| Original code we overwrote, but esp incremented 4 due to call return address on stack
-                                                 0xC3                           // RETN
-                    };
-
-                    patchCode1[1] = (byte)codeAddress.ToInt32();
-                    patchCode1[2] = (byte)(codeAddress.ToInt32() >> 8);
-                    patchCode1[3] = (byte)(codeAddress.ToInt32() >> 16);
-                    patchCode1[4] = (byte)(codeAddress.ToInt32() >> 24);
-
-                    int codeAddress2 = (codeAddress.ToInt32() + 4);
-
-                    patchCode1[12] = (byte)codeAddress2;
-                    patchCode1[13] = (byte)(codeAddress2 >> 8);
-                    patchCode1[14] = (byte)(codeAddress2 >> 16);
-                    patchCode1[15] = (byte)(codeAddress2 >> 24);
-
-
-                    int patchAddress = codeAddress.ToInt32() + 8;
-
-                    Memory.Write( hProcess.DangerousGetHandle(), (IntPtr)patchAddress, patchCode1, true );
-
-                    byte[] patchCode2 = new byte[] { 0xB8, 0x00, 0x00, 0x00, 0x00, // MOV EAX, <patchAddress>
-                                                   0xFF, 0xD0,                   // CALL EAX
-                                                   0x90                          // NOP
-                    };
-
-                    patchCode2[1] = (byte)patchAddress;
-                    patchCode2[2] = (byte)(patchAddress >> 8);
-                    patchCode2[3] = (byte)(patchAddress >> 16);
-                    patchCode2[4] = (byte)(patchAddress >> 24);
-
-                    IntPtr writeAddress = new IntPtr( (baseAddress.ToInt32() + 0x1000) + offset );
-
-                    Memory.Write( hProcess.DangerousGetHandle(), writeAddress, patchCode2, true );
-
-                    if (Win32.ResumeThread( hThread.DangerousGetHandle() ) == -1)
-                    {
-                        UOM.SetStatusLabel( Strings.ResumeThreadfailed );
-                        hProcess.Dispose();
-                        hThread.Dispose();
-                        return false;
-                    }
-
-                    byte[] clientPidBytes = new byte[4];
-                    do
-                    {
-                        Memory.Read( hProcess.DangerousGetHandle(), (IntPtr)codeAddress, clientPidBytes, false );
-                        clientPid = clientPidBytes[3] << 24 | clientPidBytes[2] << 16 | clientPidBytes[1] << 8 | clientPidBytes[0];
-                    } while (clientPid == 0);
-                }
-
-                hProcess.Close();
-                hThread.Close();
-                return ClientLauncher.Attach( (uint)clientPid, options, true, out index );
-            }
-            UOM.SetStatusLabel( Strings.Processcreationfailed );
-            return false;
-        }
-
-        private static IntPtr GetBaseAddress( Win32.SafeProcessHandle hProcess, Win32.SafeThreadHandle hThread )
-        {
-            /* Entry point varies, so need to use GetThreadContext to find entrypoint */
-            Win32.CONTEXT cntx = new Win32.CONTEXT();
-            cntx.ContextFlags = (int)Win32.CONTEXT_FLAGS.CONTEXT_FULL;
-            Win32.GetThreadContext( hThread.DangerousGetHandle(), ref cntx );
-
-            /* Ebx+8 = pointer to entrypoint to be read with ReadProcessMemory. */
-            byte[] tmp = new byte[4];
-            Memory.Read( hProcess.DangerousGetHandle(), (IntPtr)(cntx.Ebx + 8), tmp, true );
-
-            int baseAddress = tmp[3] << 24 | tmp[2] << 16 | tmp[1] << 8 | tmp[0];
-            return (IntPtr)baseAddress;
+            Load(options.UOClientPath, options.UOFolder, Path.Combine(options.UOSFolder, "UOS.dll"), options.UOSFolder, "Install", param.ToString(), out pid);
+            return ClientLauncher.Attach(pid, options, true, out index);
         }
     }
 }
